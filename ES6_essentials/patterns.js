98cc2d2b7a1d89edbb66bef2c69320e499542916
Joao Palma <jpalmaneto@gmail.com>
2020-07-06 19:08:00 -0300

    constructor pattern

---
+++ ES6_essentials/patterns.js
@@ -0,0 +1,19 @@
+//Constructor pattern -> compatível com ES5 
+function Bicho(grupo, numeros){
+    this.grupo = grupo;
+    this.numeros = numeros; 
+}
+
+
+Bicho.prototype.showNumeros = function(){
+    this.numeros.forEach(element => {
+        console.log(element)
+    });
+}
+
+const Burro = new Bicho(3, [31,32,33,34]);
+const a = Burro.showNumeros();
+console.log(a);
+
+//module pattern
+
