242aa95ef613765d26fd40d5c743fe042397c2b1
Joao Palma <jpalmaneto@gmail.com>
2020-08-03 19:53:30 -0300

    arrays

---
+++ ES6_essentials/arrays.js
@@ -0,0 +1,17 @@
+const a = [1,3,5];
+
+//retira iten da última posição 
+a.pop();
+console.log(a);
+
+//adiciona item na última posição
+a.push(7);
+console.log(a);
+
+//retira iten da primeira posição
+a.shift();
+console.log(a);
+
+//adiciona item na primeira posição
+a.unshift("a");
+console.log(a); 
